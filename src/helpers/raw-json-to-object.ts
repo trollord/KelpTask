30a3965254744951785a3ebea979ba124ee5dc54
trollord <mskasan30@gmail.com>
2024-07-24 18:22:49 +0530

    refactoring code

--- src/helpers/raw-json-to-object.ts
+++ src/helpers/raw-json-to-object.ts
@@ -1,30 +1,23 @@
-function createObject(obj) {
-  const result = {};
+export const convertRawJsonToObject = (rawJson) => {
+  return rawJson.map((obj) => {
+    const result = {};
 
-  for (const path in obj) {
-    const parts = path.split('.');
-    let current = result;
+    for (const path in obj) {
+      const parts = path.split('.');
+      let current = result;
 
-    for (let i = 0; i < parts.length; i++) {
-      const part = parts[i];
+      for (let i = 0; i < parts.length; i++) {
+        const part = parts[i];
 
-      if (i === parts.length - 1) {
-        current[part] = obj[path];
-      } else {
-        current[part] = current[part] || {};
-        current = current[part];
+        if (i === parts.length - 1) {
+          current[part] = obj[path];
+        } else {
+          current[part] = current[part] || {};
+          current = current[part];
+        }
       }
     }
-  }
 
-  return result;
-}
-
-export const convertRawJsonToObject = async (rawJson) => {
-  const result = [];
-  for (const obj of rawJson) {
-    const newObject = createObject(obj);
-    result.push(newObject);
-  }
-  return result;
+    return result;
+  });
 };
